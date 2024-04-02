f7a31f546f9942e3bdac3c267514a5d197e91f47
ManishiMehra <manishimmehra@gmail.com>
2024-04-02 12:36:05 +0530

    header is integrated with git init

---
+++ src/CommonComponent/Card/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const CardComponent = () => {
+    return ( 
+        <h1 style={{marginTop:60}}>Card Component</h1>
+     );
+}
+ 
+export default CardComponent;
---
+++ src/Component/Productslist/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import CardComponent from "../../CommonComponent/Card";
+import { Grid } from "@mui/material";
+
+const ProductsList = () => {
+  return (
+    //         <Grid container spacing={2}>
+    //   <Grid item xs={4} md={4}>
+    //   <CardComponent />
+    //   </Grid>
+    //   <Grid item xs={4} md={4}>
+    //   <CardComponent />
+    //   </Grid>
+    //   <Grid item xs={4} md={4}>
+    //   <CardComponent />
+    //   </Grid>
+    // </Grid>
+    <div>
+      <CardComponent />
+    </div>
+  );
+};
+
+export default ProductsList;
